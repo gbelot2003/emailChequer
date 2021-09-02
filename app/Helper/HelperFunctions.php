bb28f90fd55f09bd9b1aeb21a7f5825e0dfefefc
Gerardo Belot <gbelot2003@hotmail.com>
2021-09-01 23:02:40 -0600

    :art: Separating helper function for further use

---
+++ app/Helper/HelperFunctions.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Helper;
+
+class HelperFunctions
+{
+    public function generateRandomString($length = 10) {
+        return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
+    }
+}
