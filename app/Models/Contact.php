fd8bd954b1f006c63f9f786f10beddfbac4bf96f
Gerardo Belot <gbelot2003@hotmail.com>
2021-09-09 23:26:26 -0600

    Many changes

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -5,9 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Reciver extends Model
+class Contact extends Model
 {
     use HasFactory;
-
-    protected $fillable = ['email', 'name'];
 }
--- database/migrations/2021_09_04_230434_create_categories_table.php
+++ database/migrations/2021_09_04_230434_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateReciversTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateReciversTable extends Migration
      */
     public function up()
     {
-        Schema::create('recivers', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateReciversTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('recivers');
+        Schema::dropIfExists('categories');
     }
 }
